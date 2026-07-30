8ec3101d07fc7c427beecd8d737787dfd1c27add
Kenshin <alipiokyle44@gmail.com>
2026-07-30 12:18:34 -0400

    Initial commit - FSYSTEM Laravel Docker

--- database/migrations/2025_08_18_082220_create_parents_acoount_table.php
+++ database/migrations/2025_08_18_082220_create_parents_acoount_table.php
@@ -6,27 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('parents_account', function (Blueprint $table) {
             $table->id();
+
             $table->unsignedBigInteger('parents_profile_id')->nullable();
+
+            $table->foreign('parents_profile_id')
+                  ->references('id')
+                  ->on('parents_profile')
+                  ->onDelete('cascade');
+
             $table->string('name');
             $table->string('username')->unique();
             $table->string('password');
+
             $table->unsignedBigInteger('user_role_id')->nullable();
-            $table->boolean('is_active')->default(1)->comment('1=ACTIVATED, 0=DEACTIVATED');
+
+            $table->boolean('is_active')
+                  ->default(1)
+                  ->comment('1=ACTIVATED, 0=DEACTIVATED');
+
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('parents_account');

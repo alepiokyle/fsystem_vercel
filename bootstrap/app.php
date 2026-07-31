bc2f756a4441c0ad2ed369949d6a54a15137c972
Kenshin <alipiokyle44@gmail.com>
2026-07-31 04:17:48 -0400

    Configure trustProxies and force https for login forms

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Trust Render's reverse proxy so forms generate https:// URLs
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
